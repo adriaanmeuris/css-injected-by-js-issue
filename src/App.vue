0b2e428e8d5b30e76f4b0d907cc317bcd41668ec
Adriaan Meuris <adriaan.meuris@gmail.com>
2023-03-10 10:33:30 +0100

    include from library

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,11 @@
 <script setup>
-// Source include
-// import { Comp1 } from './components/components.js'; // Component 2 CSS included
-// import Comp1 from './components/Comp1.vue'; // Component 2 CSS not included
-
-// Dist include
 import { Comp1 } from '../dist/lib/lib.js'; // Component 2 included
 // import Comp1 from '../dist/lib/Comp1.js' // Component 2 not included
 </script>
 
 <template>
   <div>
-    Component 2 styles should not be part of final bundle
+    Component 2 should not be part of final bundle when using named imports
     <br>
     <Comp1 />
   </div>
